6b2106171eb43369478369924dafe76798574b19
MalinSpaniol <mspaniol@uos.de>
2019-07-27 11:26:11 +0200

    changes to apply multidropdown trials

--- 06_main.js
+++ 06_main.js
@@ -1,11 +1,12 @@
 // In this file you initialize and configure your experiment using babeInit
 
-$("document").ready(function() {
+$("document")
+  .ready(function () {
     // prevent scrolling when space is pressed
-    window.onkeydown = function(e) {
-        if (e.keyCode === 32 && e.target === document.body) {
-            e.preventDefault();
-        }
+    window.onkeydown = function (e) {
+      if (e.keyCode === 32 && e.target === document.body) {
+        e.preventDefault();
+      }
     };
 
     // calls babeInit
@@ -13,34 +14,34 @@ $("document").ready(function() {
     // e.g. >> window.babe_monitor or window.babe_monitor.findNextView()
     // in all other modes null will be returned
     window.babe_monitor = babeInit({
-        // You have to specify all views you want to use in this experiment and the order of them
-        views_seq: [
+      // You have to specify all views you want to use in this experiment and the order of them
+      views_seq: [
             intro,
             instructions,
-            forced_choice_2A,
+            multi_dropdown,
             post_test,
             thanks,
         ],
-        // Here, you can specify all information for the deployment
-        deploy: {
-            experimentID: "INSERT_A_NUMBER",
-            serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
-            // Possible deployment methods are:
-            // "debug" and "directLink"
-            // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
-            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
-        },
-        // Here, you can specify how the progress bar should look like
-        progress_bar: {
-            in: [
+      // Here, you can specify all information for the deployment
+      deploy: {
+        experimentID: "INSERT_A_NUMBER",
+        serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
+        // Possible deployment methods are:
+        // "debug" and "directLink"
+        // As well as "MTurk", "MTurkSandbox" and "Prolific"
+        deployMethod: "debug",
+        contact_email: "YOUREMAIL@wherelifeisgreat.you",
+        prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+      },
+      // Here, you can specify how the progress bar should look like
+      progress_bar: {
+        in: [
                 // list the view-names of the views for which you want a progress bar
                 forced_choice_2A.name,
             ],
-             // Possible styles are "default", "separate" and "chunks"
-            style: "separate",
-            width: 100
-        }
+        // Possible styles are "default", "separate" and "chunks"
+        style: "separate",
+        width: 100
+      }
     });
-});
+  });
