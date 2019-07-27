a7b6cc5cc2991f0306730fb30656257d96f91e6c
MalinSpaniol <mspaniol@uos.de>
2019-07-27 11:10:08 +0200

    creation of multi-dropdown for this purpose

--- 02_custom_functions.js
+++ 02_custom_functions.js
@@ -159,3 +159,119 @@ for (var r = 0; r < rows; r++) {
 // '</style>'
 
 document.write('<table border=1>' + table + '<table>')
+
+
+
+// Here, we will define some generator functions for a multi-dropdown view
+const multi_dropdown_gens = {
+  // A generator for our view template
+  stimulus_container_gen: function (config, CT) {
+    return `<div class='babe-view'>
+                <h1 class='babe-view-title'>${config.title}</h1>
+                <p class='babe-view-question babe-view-qud'>${config.data[CT].QUD}</p>
+                <p class='babe-response-keypress-header' id = 'reminder'></p>
+                </div>`;
+  },
+  // A generator for the answer container
+  answer_container_gen: function (config, CT) {
+    return `<div class='babe-view-answer-container babe-response-dropdown'>
+                ${config.data[CT].sentence_chunk_1}
+                <select id='response1' name='answer_1'>
+                    <option disabled selected></option>
+                    <option value=${config.data[CT].choice_options_1[0]}>${config.data[CT].choice_options_1[0]}</option>
+                    <option value=${config.data[CT].choice_options_1[1]}>${config.data[CT].choice_options_1[1]}</option>
+                    <option value=${config.data[CT].choice_options_1[2]}>${config.data[CT].choice_options_1[2]}</option>
+                    <option value=${config.data[CT].choice_options_1[3]}>${config.data[CT].choice_options_1[3]}</option>
+                    <option value=${config.data[CT].choice_options_1[4]}>${config.data[CT].choice_options_1[4]}</option>
+                </select>
+                ${config.data[CT].sentence_chunk_2}
+                <select id='response2' name='answer_2'>
+                    <option disabled selected></option>
+                    <option value=${config.data[CT].choice_options_2[0]}>${config.data[CT].choice_options_2[0]}</option>
+                    <option value=${config.data[CT].choice_options_2[1]}>${config.data[CT].choice_options_2[1]}</option>
+                    <option value=${config.data[CT].choice_options_2[2]}>${config.data[CT].choice_options_2[2]}</option>
+                    <option value=${config.data[CT].choice_options_2[3]}>${config.data[CT].choice_options_2[3]}</option>
+                    <option value=${config.data[CT].choice_options_2[4]}>${config.data[CT].choice_options_2[4]}</option>
+                </select>
+                ${config.data[CT].sentence_chunk_3}
+                <select id='response2' name='answer_3'>
+                    <option disabled selected></option>
+                    <option value=${config.data[CT].choice_options_3[0]}>${config.data[CT].choice_options_3[0]}</option>
+                    <option value=${config.data[CT].choice_options_3[1]}>${config.data[CT].choice_options_3[1]}</option>
+                </select>
+                ${config.data[CT].sentence_chunk_4}
+                </p>
+                <button id='next' class='babe-view-button babe-nodisplay'>Next</button>
+            </div>`;
+  },
+  // A generator for the enable response function
+  handle_response_function: function (config, CT, babe, answer_container_generator, startingTime) {
+    let response1;
+    let response2;
+    let response3;
+
+    $(".babe-view")
+      .append(answer_container_generator(config, CT));
+
+    response1 = $("#response1");
+    response2 = $("#response2");
+    response3 = $("#response3");
+
+    // flags to check if dropdown menus have been used
+    let response_flags = [0, 0];
+
+    const display_button_checker = function (response_number) {
+      response_flags[response_number] = 1;
+      if (_.min(response_flags) === 1) {
+        $("#next")
+          .removeClass("babe-nodisplay");
+      }
+    };
+
+    response1.on("change", function () {
+      response_flags[0] = 1;
+      display_button_checker(0);
+    });
+    response2.on("change", function () {
+      response_flags[1] = 1;
+      display_button_checker(1);
+    });
+    response3.on("change", function () {
+      response_flags[2] = 1;
+      display_button_checker(2);
+    });
+
+
+    $("#next")
+      .on("click", function () {
+        const RT = Date.now() - startingTime; // measure RT before anything else
+        // clear old timeouts and remove them from the timeout array
+        clearTimeout(window.timeout[0]);
+        window.timeout.shift();
+        let trial_data = {
+          trial_name: config.name,
+          trial_number: CT + 1,
+          sentence_frame: config.data[CT].sentence_chunk_1
+            .concat("...")
+            .concat(config.data[CT].sentence_chunk_2)
+            .concat("...")
+            .concat(config.data[CT].sentence_chunk_3)
+            .concat("...")
+            .concat(config.data[CT].sentence_chunk_4),
+          response_1: $(response1)
+            .val(),
+          response_2: $(response2)
+            .val(),
+          response_3: $(response3)
+            .val(),
+          RT: RT
+        };
+
+        trial_data = babeUtils.view.save_config_trial_data(config.data[CT], trial_data);
+
+        babe.trial_data.push(trial_data);
+        babe.findNextView();
+      });
+  }
+
+};
