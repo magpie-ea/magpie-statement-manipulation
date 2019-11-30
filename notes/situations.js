3a09c2939eb6742abdab8308f08691e96e7c42e0
michael-franke <mchfranke@gmail.com>
2019-11-30 16:12:01 +0100

    added specification of situations

---
+++ notes/situations.js
@@ -0,0 +1,9 @@
+var situations = [
+  { 
+      sequence: ["ACCCD", "ACCDD", "AACDD", "AACCD", "ABCDD", "ABCCD"],
+      truth_table: [1,2,2,3,342,34],
+      instances: [
+          {n_col: 12, rows: [12,12,12,3,3,3,3,3,3,0,0,0]}
+      ]
+  }
+];
